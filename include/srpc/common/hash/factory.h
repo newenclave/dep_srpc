0ffb2417384f58f4eff1ea570f07c4e7fefd925b
newenclave <pikrat.acid@gmail.com>
2016-12-13 14:14:33 +0300

    hash

---
+++ include/srpc/common/hash/factory.h
@@ -0,0 +1,20 @@
+#ifndef SRPC_COMMON_HASH_FACTORY_H
+#define SRPC_COMMON_HASH_FACTORY_H
+
+#include <map>
+
+#include "srpc/common/hash/interface.h"
+#include "srpc/common/config/mutex.h"
+#include "srpc/common/config/functional.h"
+
+
+namespace srpc { namespace common { namespace hash {
+    class factory {
+    public:
+        srpc::function<hash::interface *( )> creator_type;
+    private:
+        std::map<std::string, creator_type> map_;
+    };
+}}}
+
+#endif // FACTORY_H
