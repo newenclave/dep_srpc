4b3245ce510fa7ca1041bea4e77cd1b2517933f9
newenclave <pikrat.acid@gmail.com>
2016-12-15 11:18:37 +0300

    --

--- srpc_files.cmake
+++ srpc_files.cmake
@@ -1,24 +1,24 @@
 
-macro( srpc_all_headers_path OUT_LIST ADD_PATH )
+macro( srpc_all_headers_path OUT_LIST PREFIX_PATH )
     list( APPEND ${OUT_LIST}
-          ${CMAKE_CURRENT_SOURCE_DIR}/${${ADD_PATH}}/include/srpc/server
-          ${CMAKE_CURRENT_SOURCE_DIR}/${${ADD_PATH}}/include/srpc/common
-          ${CMAKE_CURRENT_SOURCE_DIR}/${${ADD_PATH}}/include/srpc/common/hash
-          ${CMAKE_CURRENT_SOURCE_DIR}/${${ADD_PATH}}/include/srpc/common/timers
-          ${CMAKE_CURRENT_SOURCE_DIR}/${${ADD_PATH}}/include/srpc/common/timers/calls
-          ${CMAKE_CURRENT_SOURCE_DIR}/${${ADD_PATH}}/include/srpc/common/sizepack
-          ${CMAKE_CURRENT_SOURCE_DIR}/${${ADD_PATH}}/include/srpc/common/queues
-          ${CMAKE_CURRENT_SOURCE_DIR}/${${ADD_PATH}}/include/srpc/common/queues/traits
-          ${CMAKE_CURRENT_SOURCE_DIR}/${${ADD_PATH}}/include/srpc/common/transport
-          ${CMAKE_CURRENT_SOURCE_DIR}/${${ADD_PATH}}/include/srpc/common/transport/delegates
-          ${CMAKE_CURRENT_SOURCE_DIR}/${${ADD_PATH}}/include/srpc/common/transport/async
-          ${CMAKE_CURRENT_SOURCE_DIR}/${${ADD_PATH}}/include/srpc/common/config
-          ${CMAKE_CURRENT_SOURCE_DIR}/${${ADD_PATH}}/include/srpc/server
-          ${CMAKE_CURRENT_SOURCE_DIR}/${${ADD_PATH}}/include/srpc/server/acceptor
-          ${CMAKE_CURRENT_SOURCE_DIR}/${${ADD_PATH}}/include/srpc/server/acceptor/async
-          ${CMAKE_CURRENT_SOURCE_DIR}/${${ADD_PATH}}/include/srpc/client
-          ${CMAKE_CURRENT_SOURCE_DIR}/${${ADD_PATH}}/include/srpc/client/connector
-          ${CMAKE_CURRENT_SOURCE_DIR}/${${ADD_PATH}}/include/srpc/client/connector/async
-          ${CMAKE_CURRENT_SOURCE_DIR}/${${ADD_PATH}}/include/srpc/client/connector/async/impl
+          ${PREFIX_PATH}/include/srpc/server
+          ${PREFIX_PATH}/include/srpc/common
+          ${PREFIX_PATH}/include/srpc/common/hash
+          ${PREFIX_PATH}/include/srpc/common/timers
+          ${PREFIX_PATH}/include/srpc/common/timers/calls
+          ${PREFIX_PATH}/include/srpc/common/sizepack
+          ${PREFIX_PATH}/include/srpc/common/queues
+          ${PREFIX_PATH}/include/srpc/common/queues/traits
+          ${PREFIX_PATH}/include/srpc/common/transport
+          ${PREFIX_PATH}/include/srpc/common/transport/delegates
+          ${PREFIX_PATH}/include/srpc/common/transport/async
+          ${PREFIX_PATH}/include/srpc/common/config
+          ${PREFIX_PATH}/include/srpc/server
+          ${PREFIX_PATH}/include/srpc/server/acceptor
+          ${PREFIX_PATH}/include/srpc/server/acceptor/async
+          ${PREFIX_PATH}/include/srpc/client
+          ${PREFIX_PATH}/include/srpc/client/connector
+          ${PREFIX_PATH}/include/srpc/client/connector/async
+          ${PREFIX_PATH}/include/srpc/client/connector/async/impl
           )
 endmacro( )
