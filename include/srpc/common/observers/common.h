565ada8b95c14f27b74b1eeb326fa0887eb03dba
newenclave <kettunen.villu@gmail.com>
2016-12-18 01:19:54 +0300

    observer, slots, traits

--- include/srpc/common/observers/common.h
+++ include/srpc/common/observers/common.h
@@ -204,7 +204,7 @@ namespace srpc { namespace common { namespace observers {
 
         public:
 
-#if CXX11_ENABLED == 1
+#if CXX11_ENABLED != 0
             subscription( subscription &&o )
             {
                 parent_list_ = o.parent_list_;
