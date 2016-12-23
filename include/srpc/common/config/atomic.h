daf51d3f7b020d7d3b596795810ecc0348c6c3d8
newenclave <pikrat.acid@gmail.com>
2016-12-23 15:17:47 +0300

    server test

--- include/srpc/common/config/atomic.h
+++ include/srpc/common/config/atomic.h
@@ -1,20 +1,21 @@
 #ifndef CONFIG_ATOMIC_H
 #define CONFIG_ATOMIC_H
 
-#if CXX11_ENABLED == 0
-
-#include "boost/atomic.hpp"
-
-namespace CONFIG_TOPNAMESPACE {
-    using boost::atomic;
-}
-
-#else
+#if CXX11_ENABLED
 
 #include <atomic>
 
 namespace CONFIG_TOPNAMESPACE {
     using std::atomic;
+}
+
+
+#else
+
+#include "boost/atomic.hpp"
+
+namespace CONFIG_TOPNAMESPACE {
+    using boost::atomic;
 }
 
 #endif
