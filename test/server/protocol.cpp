a6c6a1b3a72a5fea540e1471ff6d44946ca62354
newenclave <pikrat.acid@gmail.com>
2016-12-24 16:33:07 +0300

    some test

--- test/server/protocol.cpp
+++ test/server/protocol.cpp
@@ -2,11 +2,22 @@
 
 #include "srpc/common/queues/condition.h"
 #include "srpc/common/config/atomic.h"
+#include "srpc/common/transport/delegates/message.h"
+#include "srpc/common/transport/interface.h"
+#include "srpc/common/sizepack/varint.h"
+
+using namespace srpc::common;
+
+typedef transport::delegates::message<sizepack::varint<size_t> > mess_delegate;
 
 template <typename MessageType>
-class protocol_processor {
+class message_processor: public mess_delegate {
 
 public:
+
+    typedef transport::interface::error_code        error_code;
+    typedef transport::interface *                  transport_ptr;
+    typedef srpc::shared_ptr<transport::interface>  transport_sptr;
 
     typedef MessageType     message_type;
     typedef srpc::uint64_t  slot_key_type;
@@ -19,11 +30,14 @@ public:
     typedef typename queue_type::result_enum result_enum;
     typedef typename queue_type::slot_ptr slot_ptr;
 
-    protocol_processor( slot_key_type init_id )
+    message_processor( transport_ptr t, slot_key_type init_id )
         :next_id_(init_id)
-    { }
+    {
+        transport_ = t->shared_from_this( );
+    }
 
-    virtual ~protocol_processor( ) { }
+    virtual
+    ~message_processor( ) { }
 
     slot_key_type next_id( )
     {
@@ -41,16 +55,45 @@ public:
         queues_.erase_slot( slot );
     }
 
-    virtual void send_message( const message_type &msg )
-    { }
-
-    virtual void close( )
-    { }
+    transport_ptr get_transport( )
+    {
+        return transport_.get( );
+    }
 
 protected:
 
-    virtual on_message( const char *, size_t )
+    void on_message( const char *message, size_t len )
     { }
+
+    bool validate_length( size_t len )
+    {
+        return true;
+    }
+
+    void on_error( const char *message )
+    {
+
+    }
+
+    void on_need_read( )
+    {
+        transport_->read( );
+    }
+
+    void on_read_error( const error_code & )
+    {
+        transport_->close( );
+    }
+
+    void on_write_error( const error_code &)
+    {
+
+    }
+
+    void on_close( )
+    {
+        //transport_
+    }
 
     bool push_to_slot( slot_key_type id, const message_type &msg )
     {
@@ -61,13 +104,13 @@ private:
 
     srpc::atomic<slot_key_type> next_id_;
     queue_type                  queues_;
+    transport_sptr              transport_;
 };
 
 int main( int argc, char *argv[ ] )
 {
     try {
 
-        protocol_processor<std::string> pp(100);
 
     } catch ( const std::exception &ex ) {
         std::cout << "Error " << ex.what( ) << "\n";
