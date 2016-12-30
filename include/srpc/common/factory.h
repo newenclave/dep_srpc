4a166f089e83ad54a02e5fc5f81f2730cd61f8ab
newenclave <pikrat.acid@gmail.com>
2016-12-30 17:50:01 +0300

    factory

--- include/srpc/common/factory.h
+++ include/srpc/common/factory.h
@@ -1,31 +1,34 @@
-#ifndef SRPC_COMMON_HASH_FACTORY_H
-#define SRPC_COMMON_HASH_FACTORY_H
+#ifndef SRPC_COMMON_FACTORY_H
+#define SRPC_COMMON_FACTORY_H
 
 #include <map>
 
-#include "srpc/common/hash/interface.h"
 #include "srpc/common/config/mutex.h"
 #include "srpc/common/config/functional.h"
 
-namespace srpc { namespace common { namespace hash {
+namespace srpc { namespace common {
 
-    template <typename MutexType = srpc::dummy_mutex>
+    template <typename KeyType, typename ObjectType,
+              typename MutexType = srpc::dummy_mutex>
     class factory {
 
         typedef srpc::lock_guard<MutexType> locker_type;
 
     public:
 
-        typedef MutexType                            mutex_type;
-        typedef srpc::function<hash::interface *( )> producer_type;
+        typedef KeyType                          key_type;
+        typedef ObjectType                       object_type;
+        typedef MutexType                        mutex_type;
+
+        typedef srpc::function<object_type *( )> producer_type;
 
     private:
 
-        typedef std::map<std::string, producer_type> map_type;
+        typedef std::map<key_type, producer_type> map_type;
 
     public:
 
-        hash::interface *create( const std::string &name ) const
+        object_type *create( const key_type &name ) const
         {
             locker_type l(map_lock_);
             map_type::const_iterator f = map_.find( name );
@@ -35,13 +38,13 @@ namespace srpc { namespace common { namespace hash {
             return NULL;
         }
 
-        void assign_producer( const std::string &name, producer_type producer )
+        void assign( const key_type &name, producer_type producer )
         {
             locker_type l(map_lock_);
             map_[name] = producer;
         }
 
-        void erase( const std::string &name )
+        void erase( const key_type &name )
         {
             locker_type l(map_lock_);
             map_.erase( name );
@@ -51,6 +54,7 @@ namespace srpc { namespace common { namespace hash {
         map_type            map_;
         mutable mutex_type  map_lock_;
     };
-}}}
 
-#endif // SRPC_COMMON_HASH_FACTORY_H
+}}
+
+#endif // FACTORY_H
