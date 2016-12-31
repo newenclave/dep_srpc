a3f6454bb82d2df60ece57f3713249e482ad38e0
newenclave <pikrat.acid@gmail.com>
2016-12-31 16:22:56 +0300

    protobuf default protocol

--- include/srpc/common/protobuf/channel.h
+++ include/srpc/common/protobuf/channel.h
@@ -5,17 +5,67 @@
 #include "google/protobuf/descriptor.h"
 
 #include "srpc/common/config/stdint.h"
+#include "srpc/common/config/chrono.h"
 
 namespace srpc { namespace common { namespace protobuf {
 
     class channel: public google::protobuf::RpcChannel {
+
+    public:
+
+//        enum flags {
+//             DEFAULT             = 0
+//            ,DISABLE_WAIT        = 1
+//            ,USE_CONTEXT_CALL    = 1 << 1
+//            ,USE_STATIC_CONTEXT  = 1 << 2 /// only with USE_CONTEXT_CALL
+//            ,CONTEXT_NOT_REQUIRE = 1 << 3 /// only with USE_CONTEXT_CALL
+//        };
+
+        channel( )
+            :options_(0)
+            ,timeout_(0)
+        { }
+
+        virtual void set_flags( srpc::uint32_t flags )
+        {
+            options_ = flags;
+        }
+
+        virtual srpc::uint32_t get_flags( ) const
+        {
+            return options_;
+        }
+
+        void set_flag( srpc::uint32_t flag )
+        {
+            options_ |= value;
+        }
+
+        void reset_flag( srpc::uint32_t flag )
+        {
+            options_ &= (~flag);
+        }
+
+        srpc::uint64_t timeout( ) const
+        {
+            return timeout_;
+        }
+
+        void set_timeout( srpc::uint64_t new_value )
+        {
+            timeout_ = new_value;
+        }
+
+        virtual bool alive( ) const = 0;
+
 //        virtual void CallMethod(const MethodDescriptor* method,
 //                                RpcController* controller,
 //                                const Message* request,
 //                                Message* response,
 //                                Closure* done) = 0;
     private:
-
+        srpc::uint32_t options_;
+        srpc::uint64_t timeout_;
     };
 
 }}}
